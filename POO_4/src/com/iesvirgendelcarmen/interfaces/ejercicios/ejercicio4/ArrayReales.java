d6b5bdeee35ba94edfe7ebcf06e009c529beaaf5
Izan <ios2asir@gmail.com>
2018-04-12 12:46:33 +0200

    Correcciones

--- POO_4/src/com/iesvirgendelcarmen/interfaces/ejercicios/ejercicio4/ArrayReales.java
+++ POO_4/src/com/iesvirgendelcarmen/interfaces/ejercicios/ejercicio4/ArrayReales.java
@@ -6,6 +6,16 @@ import java.util.ArrayList;
 public class ArrayReales implements Estadisticas {
 
 	private List<Double> listaDeValores = new ArrayList<>();
+	private double[] coleccionDouble;
+	
+	
+	public ArrayReales(double[] coleccionDouble) {
+		this.coleccionDouble = coleccionDouble;
+	}
+
+	public ArrayReales() {
+		coleccionDouble = new double[10];
+	}
 	
 	public boolean annadirValores(double valor){
 		return listaDeValores.add(valor);
@@ -14,12 +24,21 @@ public class ArrayReales implements Estadisticas {
 	@Override
 	public double minimo() {
 		double minimo = listaDeValores.get(0);
-		
 		for (int i = 0; i < listaDeValores.size(); i++) {
 			if(listaDeValores.get(i)<minimo)
 				minimo=listaDeValores.get(i);
 		}
 		return minimo;
+	}
+	
+	@Override
+	public double minimoColeccion() {
+		double menor = coleccionDouble[0];
+		for (int i = 0; i < coleccionDouble.length; i++) {
+			if(coleccionDouble[i]>menor)
+				menor=coleccionDouble[i];
+		}
+		return menor;
 	}
 
 	@Override
@@ -33,11 +52,29 @@ public class ArrayReales implements Estadisticas {
 	}
 
 	@Override
+	public double maximoColeccion() {
+		double maximo = coleccionDouble[0];
+		for (int i = 0; i < coleccionDouble.length; i++) {
+			if(coleccionDouble[i]>maximo)
+				maximo=coleccionDouble[i];
+		}
+		return maximo;
+	}
+
+	@Override
 	public double sumatorio() {
 		double suma=0;
-		
 		for (int i = 0; i < listaDeValores.size(); i++) {
 			suma+=listaDeValores.get(i);
+		}
+		return suma;
+	}
+	
+	@Override
+	public double sumatorioColeccion() {
+		double suma=0;
+		for (int i = 0; i < coleccionDouble.length; i++) {
+			suma+=coleccionDouble[i];
 		}
 		return suma;
 	}
--- POO_4/src/com/iesvirgendelcarmen/interfaces/ejercicios/ejercicio4/Estadisticas.java
+++ POO_4/src/com/iesvirgendelcarmen/interfaces/ejercicios/ejercicio4/Estadisticas.java
@@ -2,6 +2,10 @@ package com.iesvirgendelcarmen.interfaces.ejercicios.ejercicio4;
 
 public interface Estadisticas {
 	double minimo();
+	public double minimoColeccion();
 	double maximo();
+	double maximoColeccion();
 	double sumatorio();
+	double sumatorioColeccion();
+	
 }
